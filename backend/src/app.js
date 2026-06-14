7f8210e380b8efea742bff5286511d14871e5404
abhas-paul <apodul456@gmail.com>
2026-06-14 20:19:28 +0530

    feat: add CORS support and update Login component for authentication handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const authRouter = require("./routes/auth.route.js");
 
@@ -7,6 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRouter);
 
--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,10 +1,30 @@
 import { Link } from 'react-router';
+import { useState } from 'react';
+
 import '../auth.form.scss';
+import { useAuth } from '../hooks/useAuth';
 
 function Login() {
 
-    const handleSubmit = (e) => {
+    const { loading, handleLogin } = useAuth();
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
+
+        await handleLogin({ email, password });
+
+        
+    }
+
+    if (loading) {
+        return (
+            <main>
+                <h1>Loading....</h1>
+            </main>
+        )
     }
 
     return (
@@ -18,12 +38,16 @@ function Login() {
 
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter email address" required />
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="email" id="email" name="email" placeholder="Enter email address" required />
                     </div>
 
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter password" required />
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="password" id="password" name="password" placeholder="Enter password" required />
                     </div>
 
                     <button type="submit" className="button primary-button">Login</button>
